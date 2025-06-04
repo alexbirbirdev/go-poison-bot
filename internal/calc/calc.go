18966448bb1039dad47c359904c8cf8dee632898
Alex <alexbirdev@gmail.com>
2025-06-04 12:12:30 +0300

    Переменные формулы вынесены в .env

--- internal/calc/calc.go
+++ internal/calc/calc.go
@@ -11,10 +11,18 @@ func YuanToRub(yuan, rate float64) float64 {
 	if err != nil {
 		delta = 0.0
 	}
+	delStr := os.Getenv("CALC_DELIVERY")
+	delivery, err := strconv.ParseFloat(delStr, 64)
+	if err != nil {
+		delivery = 0.0
+	}
+	comStr := os.Getenv("CALC_COMMISSION")
+	commission, err := strconv.ParseFloat(comStr, 64)
+	if err != nil {
+		commission = 0.0
+	}
 	exchangeRate := rate + delta
-	delivery := 2000.0
-	comission := 1000.0
 
-	rubPrice := yuan*exchangeRate + delivery + comission
+	rubPrice := yuan*exchangeRate + delivery + commission
 	return rubPrice
 }
